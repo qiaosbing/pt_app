fc62d7aafe7be3bf9007b7c17f73b030ab56bdff
qiao_1998 <13014721444@163.com>
2019-08-06 10:54:44 +0800

    API搭建

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -1,4 +1,11 @@
 class UserV1Api < Grape::API
   format :json
 
+  params do
+    requires :user_no, type: String, desc: "用户名"
+    requires :user_pw, type: String, desc: "用户密码"
+  end
+  post '/' do
+
+  end
 end
