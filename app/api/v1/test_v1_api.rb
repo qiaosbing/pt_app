e715e287c4d6a21dce1b990290c1e7770746a02d
qiao_1998 <13014721444@163.com>
2019-08-06 10:50:36 +0800

    API搭建

--- app/api/v1/test_v1_api.rb
+++ app/api/v1/test_v1_api.rb
@@ -4,7 +4,7 @@ require 'grape-swagger'
 class TestV1API < Grape::API
   format :json
   Rails.logger.info "====API"
-  mount UserV1Api => "user"
+  #mount UserV1Api => "user"
 
   add_swagger_documentation base_path: '/api/v1', hide_format: true
 
