cb494c62e56255015ac57a6b89d54e923d1cb672
qiao_1998 <13014721444@163.com>
2019-08-06 11:11:41 +0800

    API搭建

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module AirApp
     #API接口路径
     config.paths.add"app/api",glob:"**/*.rb"
     config.autoload_paths+=Dir["#{Rails.root}/app/api/*"]
+
+    #sidekiq
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
