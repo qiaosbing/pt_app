428a2d8b3bedc3123b25c4885b0644950f33c87a
qiao_1998 <13014721444@163.com>
2019-08-06 14:45:54 +0800

    fix:db文件

--- app/api/v1/test_v1_api.rb
+++ app/api/v1/test_v1_api.rb
@@ -4,8 +4,7 @@ require 'grape-swagger'
 class TestV1API < Grape::API
   format :json
 
-  mount UserV1Api => "user"
+  mount UserV1Api => "user" #用户登录
 
   add_swagger_documentation base_path: '/api/v1', hide_format: true
-
 end
