8a76f828887c92437005991714713d19b91e5a8e
qiao_1998 <13014721444@163.com>
2019-08-13 15:28:32 +0800

    fix:配置

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :users
   resources :microposts
-  resources :user_descs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   mount Sidekiq::Web => '/sidekiq_web' #sidekiq
