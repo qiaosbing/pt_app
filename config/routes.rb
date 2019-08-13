1c3ae08344da50ead78c254c8a69f751524d05a0
Your Name <you@example.com>
2019-08-13 15:24:30 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  resources :users
+  resources :microposts
   resources :user_descs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
