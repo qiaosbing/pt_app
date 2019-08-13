89fa9bab0ab280d269e727477245b9cf59dbca8f
qiao_1998 <13014721444@163.com>
2019-08-13 18:05:54 +0800

    fix：配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :user_login
+
   def user_login
     Rails.logger.info "========1111111111111"
   end
