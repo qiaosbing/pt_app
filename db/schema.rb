c97bde81757a828e34f6dcb95057472380b379a6
Your Name <you@example.com>
2019-08-13 15:21:46 +0800

    fix:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_071855) do
+ActiveRecord::Schema.define(version: 2019_08_13_072126) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2019_08_13_071855) do
     t.string "mailbox"
     t.string "units"
     t.string "remark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
