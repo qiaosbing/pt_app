b3829622874d90b42fef71a9eddd4ae69cb57109
qiao_1998 <13014721444@163.com>
2019-08-15 14:49:47 +0800

    fix：登录页面

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class UserLoggerController < ApplicationController
+
+  #用户登录页面
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
-  resources :users
-  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   mount Sidekiq::Web => '/sidekiq_web' #sidekiq
   mount TestV1API => "/api/v1" #API接口
+
+
+  resources :users
+  resources :microposts
+  resources :sessions #用户登录
+
+
 end
