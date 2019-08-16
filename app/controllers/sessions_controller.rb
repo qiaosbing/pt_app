c67b2dd92092078183702f41d249311516e915d1
qiao_1998 <13014721444@163.com>
2019-08-16 16:38:57 +0800

    fix：登陆页面

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   end
   #用户登录页面
   def new
-    params[:login_no]
-    #render "登录成功"
+    user = params[:user]
+    pwd = params[:pwd]
+    Rails.logger.info "=====用户===#{user}=====密码==#{pwd}"
+    render "SUCCESS"
   end
 end
