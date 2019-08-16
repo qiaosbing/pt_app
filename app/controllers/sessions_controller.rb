45d2d5c0423dfdd132d55ef7faddcc9e37d9690b
qiao_1998 <13014721444@163.com>
2019-08-16 17:01:23 +0800

    fix：登陆页面

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
 
   def index_table
+    user = params[:user]
+    pwd = params[:pwd]
+    Rails.logger.info "===index_table==用户===#{user}=====密码==#{pwd}"
 
     render :partial => 'index_table', :layout => false
   end
@@ -8,7 +11,7 @@ class SessionsController < ApplicationController
   def new
     user = params[:user]
     pwd = params[:pwd]
-    Rails.logger.info "=====用户===#{user}=====密码==#{pwd}"
+    Rails.logger.info "==new===用户===#{user}=====密码==#{pwd}"
     # render json: {data:"SUCCESS"}, status: :ok, layout: false
 
     #render "SUCCESS"
