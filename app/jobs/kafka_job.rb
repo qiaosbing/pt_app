d10cad66b5a7b5a0485f7233eeea07bf6c8e1d4c
qiao_1998 <13014721444@163.com>
2019-08-30 16:47:57 +0800

    Gem kafka

--- app/jobs/kafka_job.rb
+++ app/jobs/kafka_job.rb
@@ -3,11 +3,11 @@ class KafkaJob < ActiveJob::Base
 
   def perform(*args)
     Rails.logger.info "==kafka测试==="
-    # event = {order_id: "1", amount: "2",timestamp: "3"}
-
-    kafka = Kafka.new(["kafka:9092"])
-
-    Rails.logger.info "======Tpoic=======#{kafka.topics}"
-    #$kafka_producer.produce(event.to_jsom, topic: "test")
+    event = {
+        order_id: "1",
+        amount: "2",
+        timestamp: "3",
+    }
+    $kafka_producer.produce(event.to_json, topic: "test")
   end
 end
