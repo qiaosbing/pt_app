6fbeede2c36d82cd7c90e08006120f315d12350f
qiao_1998 <13014721444@163.com>
2019-08-30 16:29:26 +0800

    Gem kafka

--- app/jobs/kafka_job.rb
+++ app/jobs/kafka_job.rb
@@ -3,18 +3,27 @@ class KafkaJob < ActiveJob::Base
 
   def perform(*args)
     Rails.logger.info "==kafka测试==="
-    kafka = Kafka.new(["kafka1:9092", "kafka2:9092"], client_id: "my-application")
-    Rails.logger.info "==链接topic#{kafka.inspect}==="
 
-    #实例化一个新的生产者。
-    producer = kafka.producer
-    Rails.logger.info "==新的生产者：#{producer.inspect}==="
-    #将消息添加到生成器缓冲区。
-    producer.produce("hello1", topic: "test")
-    #将消息发送给Kafka。
-    producer.deliver_messages
+    event = {
+        order_id: "1",
+        amount: "2",
+        timestamp: "3",
+    }
 
-    #kafka.deliver_message("Hello, World!", topic: "test")
+    Rails.logger.info "======#{$kafka_producer.inspect}"
+    $kafka_producer.produce(event.to_json, topic: "test")
+    # kafka = Kafka.new(["kafka1:9092", "kafka2:9092"], client_id: "my-application")
+    # Rails.logger.info "==链接topic#{kafka.inspect}==="
+    #
+    # #实例化一个新的生产者。
+    # producer = kafka.producer
+    # Rails.logger.info "==新的生产者：#{producer.inspect}==="
+    # #将消息添加到生成器缓冲区。
+    # producer.produce("hello1", topic: "test")
+    # #将消息发送给Kafka。
+    # producer.deliver_messages
+    #
+    # #kafka.deliver_message("Hello, World!", topic: "test")
     Rails.logger.info "==测试结束!==="
 
   end
