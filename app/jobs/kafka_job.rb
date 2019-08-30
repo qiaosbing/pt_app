53715c16bcf53e626079863086bcd2b0a1038f30
qiao_1998 <13014721444@163.com>
2019-08-30 15:45:05 +0800

    Gem kafka

--- app/jobs/kafka_job.rb
+++ app/jobs/kafka_job.rb
@@ -4,17 +4,17 @@ class KafkaJob < ActiveJob::Base
   def perform(*args)
     Rails.logger.info "==kafka测试==="
     kafka = Kafka.new(["kafka1:9092", "kafka2:9092"], client_id: "my-application")
-
     Rails.logger.info "==链接topic#{kafka.inspect}==="
-    kafka.deliver_message("Hello, World!", topic: "test")
+
+    #实例化一个新的生产者。
+    producer = kafka.producer
+    #将消息添加到生成器缓冲区。
+    producer.produce("hello1", topic: "test")
+    #将消息发送给Kafka。
+    producer.deliver_messages
+
+    #kafka.deliver_message("Hello, World!", topic: "test")
     Rails.logger.info "==测试结束!==="
 
-    # producer = Kafka::Producer.new(
-    #     :topic => 'test',
-    #     :host => host,
-    #     :port => port
-    # )
-    #
-    # producer.send([Kafka::Message.new("aaaaa")])
   end
 end
