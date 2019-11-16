a54b2b3b9bc4e426e3b61b9afcbedbfd3d176553
qiao_1998 <13014721444@163.com>
2019-11-16 16:51:31 +0800

    fix：DB效率测试

--- app/jobs/db_models_job.rb
+++ app/jobs/db_models_job.rb
@@ -8,8 +8,7 @@ class DbModelsJob < ActiveJob::Base
     #
     # end
 
-    DStation.find_each do |a|
-      Rails.logger.info "=============#{a.inspect}"
+    DStation.find_each(batch_size:1000) do |a|
 
     end
 
