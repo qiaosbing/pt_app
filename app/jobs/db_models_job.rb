e1902544ad7d00ed42baaeecceb4f4c470512407
qiao_1998 <13014721444@163.com>
2019-11-16 16:47:10 +0800

    fix

--- app/jobs/db_models_job.rb
+++ app/jobs/db_models_job.rb
@@ -3,14 +3,14 @@ class DbModelsJob < ActiveJob::Base
 
   def perform(*args)
 
-
-    DStation.all.each do |a|
-
-    end
-
-    # DStation.find_each do |a|
+    #
+    # DStation.all.each do |a|
     #
     # end
+
+    DStation.find_each do |a|
+
+    end
 
   end
 end
