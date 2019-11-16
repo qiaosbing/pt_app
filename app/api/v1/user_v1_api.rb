73111c822bc3a8846ad185ecbec117263116f43b
qiao_1998 <13014721444@163.com>
2019-11-16 17:48:19 +0800

    fix：DB效率测试

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -2,18 +2,15 @@ class UserV1Api < Grape::API
   format :json
 
   params do
-    requires :user_no, type: String, desc: "用户名"
-    requires :user_pwd, type: String, desc: "密码"
+    requires :user_no, type: String, desc: "姓名"
   end
   #用户表
   #rails generate scaffold user_desc user_id:string user_name:string phone:string mailbox:string units:string remark:string
 
   post '/' do
     user_name = params[:user_no]
-    user_pwd = params[:user_pwd]
-    return {data_arr: "滚犊子！！！！"} if !user_name.present? || !user_pwd.present?
-
-    #data_user = User.find_by("user_name = #{user_name} and user_pwd = #{user_pwd}")
+    Rails.logger.info "=====#{user_name.inspect}"
+    return {data_arr: "滚犊子！！！！"} if !"乔帅兵".eql?(user_name)
   end
 
 
