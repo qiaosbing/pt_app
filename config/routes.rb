f230c8269c37c28513cb25ee2aee2008c75b595f
qiao_1998 <13014721444@163.com>
2019-11-30 09:19:33 +0800

    fix：测试

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "root#index"
   mount Sidekiq::Web => '/sidekiq_web' #sidekiq
   mount TestV1API => "/api/v1" #API接口
 
 
-  resources :microposts
   resources :sessions do #用户登录
     get "index_table", on: :collection
   end
