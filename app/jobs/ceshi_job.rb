e26fc24fab837e29d11570a004f659754cc52009
qiao_1998 <13014721444@163.com>
2019-12-02 16:22:24 +0800

    fix

---
+++ app/jobs/ceshi_job.rb
@@ -0,0 +1,13 @@
+class CeshiJob < ActiveJob::Base
+  require 'socket'
+
+  def perform(*args)
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
+    UDPSocket.open do |s|
+      s.connect '64.233.187.99', 1
+      s.addr.last
+    end
+  ensure
+    Socket.do_not_reverse_lookup = orig
+  end
+end
--- app/worker/user_worker.rb
+++ app/worker/user_worker.rb
@@ -9,12 +9,6 @@ class UserWorker
 
 
   def perform(*args)
-    user = User.new
-    user.user_name = "天下第一山"
-    user.user_pwd = "0101"
-    user.user_session = "4396"
-    user.user_desc = "软件测试！"
 
-    user.save
   end
 end
