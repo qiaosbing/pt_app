d366e7faf9e436279deb0987d073a3937f5ff831
qiao_1998 <13014721444@163.com>
2019-12-03 18:46:44 +0800

    fix：获取本地IP

--- app/jobs/get_ip.rb
+++ app/jobs/get_ip.rb
@@ -1,7 +1,13 @@
-class CeshiJob < ActiveJob::Base
+class GetIp < ActiveJob::Base
   require 'socket'
 
+  #获取请求用户的IP
   def perform(*args)
+    ip = request.remote_ip
+  end
+
+  #获取本地ip地址
+  def ip
     orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
     UDPSocket.open do |s|
       s.connect '64.233.187.99', 1
