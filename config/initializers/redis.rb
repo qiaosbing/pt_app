163d29f74d6686a442de71736e958289ff90a28d
qiao_1998 <13014721444@163.com>
2019-12-03 19:33:46 +0800

    fix：获取本地IP

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
 #redis连接
-# 连接第一个库
 $redis0 = Redis.new(:host => 'localhost', :port => 6379, db: 0)
+$redis1 = Redis.new(:host => 'localhost', :port => 6379, db: 1)
+$redis2 = Redis.new(:host => 'localhost', :port => 6379, db: 2)
+$redis3 = Redis.new(:host => 'localhost', :port => 6379, db: 3)
