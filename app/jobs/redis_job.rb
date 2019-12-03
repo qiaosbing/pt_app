595a9b94222fc4cdc5d1493f84497a98c402dad5
qiao_1998 <13014721444@163.com>
2019-12-03 19:42:28 +0800

    fix：获取本地IP

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -3,10 +3,15 @@ class RedisJob < ActiveJob::Base
 
   def perform(*args)
     low = LowEnforcementParameter.all
+    data_time = Time.now.strftime("'%Y%m%d%H'")
     data_low = low.first(10)
+    @data_arr = []
     data_low.each do |low|
-      $redis0.select
-
+      hash = {}
+      hash[:name] = low.JCDWMC #企业名称
+      hash[:name] = low.FRMC #负责人
+      @data_arr << hash
     end
+    $redis0.set(data_time,@data_arr.to_s)
     end
   end
