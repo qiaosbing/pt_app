b3799e706d6ca8da8e396a34cfc0303b3fa4cfb8
qiao_1998 <13014721444@163.com>
2019-12-04 21:56:01 +0800

    fix：redis

--- app/jobs/redis_v_job.rb
+++ app/jobs/redis_v_job.rb
@@ -1,12 +1,14 @@
 class RedisVJob < ActiveJob::Base
 
   def perform(*args)
-      @data_arr = []
-      hash_key = $redis2.hgetall("M")
-      hash_key.each do |key|
-        @data_arr << $redis2.hget("M","#{key}")
-      end
-      Rails.logger.info "----#{@data_arr.size}"
+    # @data_arr = []
+    # hash_key = $redis2.hgetall("M")
+    # hash_key.each do |key|
+    #   @data_arr << $redis2.hget("M","#{key}")
+    # end
+    # Rails.logger.info "----#{@data_arr.size}"
+    data = DData5MinYyyymm.where(:data_time => "201928150000".."201928210000")
+    Rails.logger.info "====#{data.size}"
   end
 
 end
