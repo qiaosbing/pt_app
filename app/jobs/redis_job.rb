617054099314591953791a7e80376b94720e5d67
qiao_1998 <13014721444@163.com>
2019-12-04 15:31:48 +0800

    fix：redis测试

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -4,8 +4,8 @@ class RedisJob < ActiveJob::Base
   def perform(*args)
     time_beg = "20190715".to_time
     time_end = Time.now
-    time_1 =  time_beg.strftime('%Y%m%d%H')
-    time_2 =  time_end.strftime('%Y%m%d%H')
+    time_1 =  time_beg.strftime('%Y%m%d')
+    time_2 =  time_end.strftime('%Y%m%d')
 
     day =  time_2.to_i - time_1.to_i
     Rails.logger.info "===#{day}"
