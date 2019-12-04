3063498e21cdbe30b15f5a2d0fff7cb38deaf4a4
qiao_1998 <13014721444@163.com>
2019-12-04 16:45:10 +0800

    fix：redis测试

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -6,8 +6,9 @@ class RedisJob < ActiveJob::Base
     time_end = Time.now
     while time_beg <= time_end
       data_time = time_beg + 1.day
-      Rails.logger.info "=3==#{data_time}"
       time_beg + 1.day
+      Rails.logger.info "=3==#{time_beg}"
+
+    end
     end
   end
-end
