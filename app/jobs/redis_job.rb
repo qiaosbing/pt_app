d6df1b0570763a9629ac496fecd1e7a8dfb79b87
qiao_1998 <13014721444@163.com>
2019-12-04 22:10:41 +0800

    fix：redis

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -1,24 +1,22 @@
 class RedisJob < ActiveJob::Base
 
   def perform(*args)
-    time_beg = "20190815".to_time
-    time_end = "20190820".to_time
+    time_beg = "20190816".to_time
+    time_end = "20190816".to_time
     @top5_data_times = []
     while time_beg <= time_end
       @top5_data_times << time_beg
-      time_beg = time_beg + 1.day
+      time_beg = time_beg + 1.hour
     end
 
     @data_arr = []
     @top5_data_times.each do |time|
-      s_brg = time.beginning_of_day #今天的开始时间
-      s_end = time.at_end_of_day #今天的结束时间
-      datas = DData5MinYyyymm.where(:data_time => s_brg.strftime('%Y%m%d%H%M')..s_end.strftime('%Y%m%d%H%M'))
+      datas = DData5MinYyyymm.where(:data_time => time.strftime('%Y%m%d%H00')..time.strftime('%Y%m%d%H55'))
       @data_arr = []
       datas.each do |da|
         @data_arr << da
       end
-      $redis2.hset("M", time.strftime('%Y%m%d'), "#{@data_arr}")
+      $redis2.hset("M", time.strftime('%Y%m%d%H'), "#{@data_arr}")
     end
   end
 end
