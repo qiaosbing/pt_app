7efe9699b72481fcda20b76ba8e9b351503d1821
qiao_1998 <13014721444@163.com>
2019-12-04 21:51:56 +0800

    fix：redis

--- app/jobs/redis_v_job.rb
+++ app/jobs/redis_v_job.rb
@@ -1,7 +1,12 @@
 class RedisVJob < ActiveJob::Base
 
   def perform(*args)
-
+      @data_arr = []
+      hash_key = $redis2.hgetall("M")
+      hash_key.each do |key|
+        @data_arr << $redis2.hget("M","#{key}")
+      end
+      Rails.logger.info "----#{@data_arr.size}"
   end
 
 end
