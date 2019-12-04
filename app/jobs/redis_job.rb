4777301a6600397581d7fb336fedcf184fe9cf2e
qiao_1998 <13014721444@163.com>
2019-12-04 18:54:53 +0800

    fix：redis测试

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -15,7 +15,11 @@ class RedisJob < ActiveJob::Base
       s_brg = time.beginning_of_day #今天的开始时间
       s_end = time.at_end_of_day #今天的结束时间
       datas = DData5MinYyyymm.where(:data_time => s_brg.strftime('%Y%m%d%H%M')..s_end.strftime('%Y%m%d%H%M'))
-      $redis2.hset("M", time.strftime('%Y%m%d'), "#{datas}")
+      @data_arr = []
+      datas.each do |da|
+       Rails.logger.info "====#{da}"
+      end
+      #$redis2.hset("M", time.strftime('%Y%m%d'), "#{datas}")
     end
   end
 end
