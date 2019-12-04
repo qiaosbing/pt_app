2613376c5959634df26b1def004b151fda6a1ee5
qiao_1998 <13014721444@163.com>
2019-12-04 17:27:49 +0800

    fix：redis测试

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -2,13 +2,20 @@ class RedisJob < ActiveJob::Base
   require "redis"
 
   def perform(*args)
-    time_beg = "20190715".to_time
-    time_end = Time.now
+    time_beg = "20190801".to_time
+    time_end = "20190802".to_time
     @top5_data_times = []
     while time_beg <= time_end
-      Rails.logger.info "=====#{time_beg.strftime('%Y-%m-%d %H:00:00')}"
-      @top5_data_times << time_beg.strftime('%Y-%m-%d %H:00:00')
+      @top5_data_times << time_beg
       time_beg = time_beg + 1.day
+    end
+
+    @data_arr = []
+    @top5_data_times.each do |time|
+      s_brg = time.beginning_of_day #今天的开始时间
+      s_end = time.at_end_of_day #今天的结束时间
+      datas = DData5MinYyyymm.where(:data_time => s_brg.strftime('%Y%m%d%H%M')..s_end.strftime('%Y%m%d%H%M'))
+      $redis2.hset("M", time.strftime('%Y%m%d'), "#{datas}")
     end
   end
 end
--- app/worker/redis_worker.rb
+++ app/worker/redis_worker.rb
@@ -17,13 +17,11 @@ class UserWorker
       time_beg = time_beg + 1.day
     end
 
-
-    low_min = DData5MinYyyymm.all #获取分钟数据
     @data_arr = []
     @top5_data_times.each do |time|
       s_brg = time.beginning_of_day #今天的开始时间
       s_end = time.at_end_of_day #今天的结束时间
-      datas = low_min.where(:data_time => s_brg.strftime('%Y%m%d%H%M')..s_end.strftime('%Y%m%d%H%M'))
+      datas = DData5MinYyyymm.where(:data_time => s_brg.strftime('%Y%m%d%H%M')..s_end.strftime('%Y%m%d%H%M'))
       $redis2.hset("M", time.strftime('%Y%m%d'), "#{datas}")
     end
   end
