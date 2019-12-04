7de2788edb98bfb2157f780ec35b83c2060d490a
qiao_1998 <13014721444@163.com>
2019-12-04 15:29:46 +0800

    fix：

--- app/worker/redis_worker.rb
+++ app/worker/redis_worker.rb
@@ -9,15 +9,24 @@ class UserWorker
 
 
   def perform(*args)
-    low = DData5MinYyyymm.where()
-    @data_arr = []
-    low.each do |low|
-      hash = {}
-      hash[:name] = low.JCDWMC #企业名称
-      hash[:frmc] = low.FRMC #负责人
-      @data_arr << hash
-    end
-    $redis2.hset("M",data_time,"#{@data_arr}")
+    time_beg = "20190715".to_time
+    time_end = Time.now
+    time_1 =  time_beg.strftime('%Y%m%d%H')
+    time_2 =  time_end.strftime('%Y%m%d%H')
+
+    day =  time_2.to_i - time_1.to_i
+    Rails.logger.info "===#{day}"
+
+      # low_min = DData5MinYyyymm.all
+    # low_min.select {|x|}
+    # @data_arr = []
+    # low.each do |low|
+    #   hash = {}
+    #   hash[:name] = low.JCDWMC #企业名称
+    #   hash[:frmc] = low.FRMC #负责人
+    #   @data_arr << hash
+    # end
+    # $redis2.hset("M",data_time,"#{@data_arr}")
   end
 
 end
