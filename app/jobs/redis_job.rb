483106539469ec9106ff6027d323e1916492eaab
qiao_1998 <13014721444@163.com>
2019-12-04 17:11:34 +0800

    fix：redis测试

--- app/jobs/redis_job.rb
+++ app/jobs/redis_job.rb
@@ -6,6 +6,7 @@ class RedisJob < ActiveJob::Base
     time_end = Time.now
     @top5_data_times = []
     while time_beg <= time_end
+      Rails.logger.info "=====#{time_beg.strftime('%Y-%m-%d %H:00:00')}"
       @top5_data_times << time_beg.strftime('%Y-%m-%d %H:00:00')
       time_beg = time_beg + 1.day
     end
