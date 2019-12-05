e271cb8e990cae1bde6140a2325348b6dd062428
qiao_1998 <13014721444@163.com>
2019-12-05 11:09:24 +0800

    fix：json文件测试

--- app/jobs/read_json_job.rb
+++ app/jobs/read_json_job.rb
@@ -1,21 +1,27 @@
 class ReadJsonJob < ActiveJob::Base
 
   def perform(*args)
-    data_time = "20190915".to_time
-    #获取数据
-    data_min = DData5MinYyyymm.where(:data_time => data_time.strftime('%Y%m%d%H00')..data_time.strftime('%Y%m%d%H55'))
+    # data_time = "20190915".to_time
+    # #获取数据
+    # data_min = DData5MinYyyymm.where(:data_time => data_time.strftime('%Y%m%d%H00')..data_time.strftime('%Y%m%d%H55'))
+    # @data_arr = []
+    # data_min.each do |min|
+    #   @data_arr << min.to_json
+    # end
+    # json_name = "json_min.json" #json文件名称
+    # file_path = "#{Rails.root}/public/json" #json文件路径
+    # FileUtils.mkdir_p(file_path) unless File.exist?(file_path) #文件不存在则创建
+    #
+    # File.open(Rails.root.join("#{file_path}", json_name), 'wb') {|f| f.write(@data_arr)}
+
+    #读取json文件数据
+    data_json = File.read("#{Rails.root}/public/json/json_min.json")
+    eval(data_json) #解析数据据
+
     @data_arr = []
-    data_min.each do |min|
-      @data_arr << min.to_json
+    data_json.each do |json|
+      @data_arr << JSON.parse(json)
     end
-    json_name = "json_min.json" #json文件名称
-    file_path = "#{Rails.root}/public/json" #json文件路径
-    FileUtils.mkdir_p(file_path) unless File.exist?(file_path) #文件不存在则创建
-
-    File.open(Rails.root.join("#{file_path}", json_name), 'wb') {|f| f.write(@data_arr)}
-
-    # #读取json文件数据
-    # data_json = File.read("#{Rails.root}/public/json/json_min.json")
-    # Rails.logger.info "====#{data_json.inspect}"
+    Rails.logger.info "====#{@data_arr.inspect}"
   end
 end
