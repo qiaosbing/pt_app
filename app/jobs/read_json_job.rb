3ec23bdcd33458ffb5b2e1a036648bd8ae3e5728
qiao_1998 <13014721444@163.com>
2019-12-05 11:19:06 +0800

    fix：json文件测试

--- app/jobs/read_json_job.rb
+++ app/jobs/read_json_job.rb
@@ -1,27 +1,42 @@
 class ReadJsonJob < ActiveJob::Base
 
   def perform(*args)
-    # data_time = "20190915".to_time
-    # #获取数据
-    # data_min = DData5MinYyyymm.where(:data_time => data_time.strftime('%Y%m%d%H00')..data_time.strftime('%Y%m%d%H55'))
-    # @data_arr = []
-    # data_min.each do |min|
-    #   @data_arr << min.to_json
-    # end
-    # json_name = "json_min.json" #json文件名称
-    # file_path = "#{Rails.root}/public/json" #json文件路径
-    # FileUtils.mkdir_p(file_path) unless File.exist?(file_path) #文件不存在则创建
-    #
-    # File.open(Rails.root.join("#{file_path}", json_name), 'wb') {|f| f.write(@data_arr)}
+    #将指定数据生成json文件
+    data_time = "20190915".to_time
+    #获取数据
+    data_min = DData5MinYyyymm.where(:data_time => data_time.strftime('%Y%m%d%H00')..data_time.strftime('%Y%m%d%H55'))
+    @data_arr = []
+    data_min.each do |min|
+      @data_arr << min.to_json
+    end
+    d_size = open_data(@data_arr)
+    Rails.logger.info "====#{d_size.inspect}"
 
+      # #读取文件的路径
+    # url = "#{Rails.root}/public/json/json_min.json"
+    # #调用方法获取数据
+    # data = read_data(url)
+    # Rails.logger.info "====#{data.inspect}"
+  end
+
+  #读取json文件
+  def read_data(url)
     #读取json文件数据
-    data_json = File.read("#{Rails.root}/public/json/json_min.json")
-    data_json =  eval(data_json) #解析数据据
-
+    data_json = File.read(url)
+    #解析数据据
+    data_json = eval(data_json)
     @data_arr = []
     data_json.each do |json|
       @data_arr << JSON.parse(json)
     end
-    Rails.logger.info "====#{@data_arr.inspect}"
+    return @data_arr
+  end
+
+  #生成json文件
+  def open_data(data)
+    json_name = "json_min.json" #json文件名称
+    file_path = "#{Rails.root}/public/json" #json文件路径
+    FileUtils.mkdir_p(file_path) unless File.exist?(file_path) #文件不存在则创建
+    File.open(Rails.root.join("#{file_path}", json_name), 'wb') {|f| f.write(data)}
   end
 end
