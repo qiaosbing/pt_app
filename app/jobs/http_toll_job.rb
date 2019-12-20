7c8277bc8c2fdcf61051209f870ae053d19af698
qiao_1998 <13014721444@163.com>
2019-12-20 16:25:49 +0800

    fix：测试

--- app/jobs/http_toll_job.rb
+++ app/jobs/http_toll_job.rb
@@ -6,7 +6,7 @@ class HttpTollJob < ActiveJob::Base
     dataTime = Time.now.strftime("Y%m%d")
     dataType = "2"
     datas = {"stationCodes": stationCodes,"dataTime":dataTime,"dataType":dataType}
-    d_headers = {'Authorization': 'wuzhongYCSP 1576828704253000 d3V6aG9uZ1lDU1AxNTc2ODI4NzAzNTUxMDAw'}
+    d_headers = {'Authorization': 'wuzhongYCSP 1576830296241000 d3V6aG9uZ1lDU1AxNTc2ODI4NzAzNTUxMDAw'}
     datas = RestClient.post 'http://dpt.daqi110.com/gateway/getData/air/wf/air/data/getStationData', datas.to_json, headers = d_headers
     data_arr = JSON.parse(datas.body)
     Rails.logger.info "=====#{data_arr.inspect}"
