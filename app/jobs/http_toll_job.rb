b06b7bc2fc497a75c6928224547ae9c660ef80d9
qiao_1998 <13014721444@163.com>
2019-12-21 16:03:00 +0800

    fix：测试

--- app/jobs/http_toll_job.rb
+++ app/jobs/http_toll_job.rb
@@ -2,10 +2,13 @@ class HttpTollJob < ActiveJob::Base
   require "rjb"
 
   def perform(*args)
-    Rjb::load(classpath = '.:/root/java/wz_lkjh-1.0-SNAPSHOT.jar', jvmargs=[])
-    tararchive = Rjb::import('com.wz.main.ceshi')
+    # Rjb::load(classpath = '.:/root/java/wz_lkjh-1.0-SNAPSHOT.jar', jvmargs=[])
+    # tararchive = Rjb::import('com.wz.main.ceshi')
+    Rjb::load("/root/java/wz_lkjh-1.0-SNAPSHOT.jar");
+    frontConsume = Rjb::import("com.wz.main.ceshi");
+    frontConsume.createRequestHtml();
 
-    Rails.logger.info "----#{tararchive.inspect}"
+    Rails.logger.info "----#{frontConsume.inspect}"
   end
 
 end
