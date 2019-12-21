d1c01bcfd1d705f7487dd1fe5f525086dc5cd04a
qiao_1998 <13014721444@163.com>
2019-12-21 14:58:09 +0800

    fix：测试

--- app/jobs/http_toll_job.rb
+++ app/jobs/http_toll_job.rb
@@ -1,9 +1,9 @@
 class HttpTollJob < ActiveJob::Base
   require 'java'
-
+  require '/root/java/wz_lkjh-1.0-SNAPSHOT.jar'
   def perform(*args)
-
-    tmp = DmCodec.encodeB62("aaa")
+    java_import 'com.logstash.Main'
+    Main.exce(es_datas)
 
   end
 
