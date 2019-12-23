5dfec23724e1a31a4cb5f977f201a694f09f1402
qiao_1998 <13014721444@163.com>
2019-12-23 10:36:47 +0800

    fix：测试

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -22,42 +22,36 @@ class UserV1Api < Grape::API
     d_station = DStation.where(:station_type => "15") #获取站点信息
     @data_arr = []
     user_name.each do |x|
+      #筛选站点
       station_id = x["station_id"]
       station = d_station.select { |x| x.dz_station_id == station_id }.first
-      next if !station.present?
-      hash = {}
-      hash[:station_id] = station.id.to_s
-      hash[:station_name] = station.station_name
-      hash[:data_time] = x["data_time"]
-      hash[:avg_so2] = x["so2"]
-      hash[:so2_label] = x["so2_label"]
-      hash[:iaqi_so2] = x["iaqi_so2"]
-      hash[:avg_no2] = x["no2"]
-      hash[:no2_label] = x["no2_label"]
-      hash[:iaqi_no2] = x["iaqi_no2"]
-      hash[:avg_co] = x["co"]
-      hash[:co_label] = x["co_label"]
-      hash[:iaqi_co] = x["iaqi_co"]
-      hash[:avg_o3] = x["o3"]
-      hash[:o3_label] = x["o3_label"]
-      hash[:iaqi_o3] = x["iaqi_o3"]
-      hash[:avg_pm10] = x["pm10"]
-      hash[:pm10_label] = x["pm10_label"]
-      hash[:iaqi_ma24_pm10] = x["iaqi_pm10"]
-      hash[:avg_pm25] = x["pm25"]
-      hash[:pm25_label] = x["pm25_label"]
-      hash[:iaqi_ma24_pm25] = x["iaqi_pm25"]
-      hash[:temperature] = x["temperature"]
-      hash[:temperature_label] = x["temperature_label"]
-      hash[:humidity] = x["humidity"]
-      hash[:humidity_label] = x["humidity_label"]
-      hash[:windspeed] = x["windspeed"]
-      hash[:windspeed_label] = x["windspeed_label"]
-      hash[:winddirection] = x["winddirection"]
-      hash[:winddirection_label] = x["winddirection_label"]
-      hash[:avg_tvoc] = x["avg_tvoc"]
-      hash[:tvoc_label] = x["tvoc_label"]
-      hash[:aqi] = x["aqi"]
+      next if !station.present? && !x["aqi"].present?
+
+
+      #封装数据
+      hash = {station_id:station.id.to_s,station_name:station.station_name,data_time:x["data_time"],
+              avg_so2:x["so2"], so2_label:x["so2_label"],iaqi_so2:x["iaqi_so2"],
+              avg_no2:x["no2"],no2_label:x["no2_label"],iaqi_no2:x["iaqi_no2"],
+              avg_co:x["co"],co_label:x["co_label"],iaqi_co:x["iaqi_co"],
+              avg_o3:x["o3"],o3_label:x["o3_label"],iaqi_o3:x["iaqi_o3"],
+              avg_pm10:x["pm10"],pm10_label:x["pm10_label"],iaqi_ma24_pm10:x["iaqi_pm10"],
+              avg_pm25:x["pm25"],pm25_label:x["pm25_label"],iaqi_ma24_pm25:x["iaqi_pm25"],
+              temperature:x["temperature"],temperature_label:x["temperature_label"],
+              humidity:x["humidity"],humidity_label:x["humidity_label"],
+              windspeed:x["windspeed"],windspeed_label:x["windspeed_label"],
+              winddirection:x["winddirection"],winddirection_label: x["winddirection_label"],
+              avg_tvoc: x["avg_tvoc"],tvoc_label:x["tvoc_label"]}
+
+      #获取aqi数据
+      aqi = x["aqi"]
+      if x["aqi"] != nil && x["aqi"] != 0
+        level = SAqiLevel.find_by_aqi(aqi)
+        hash[:aqi] = aqi
+        hash[:chief_pollutant] = x["chief_pollutant"] #首要污染物
+        hash[:aqi_level] = level.aqi_level
+        hash[:aqi_class] = level.aqi_class
+        hash[:aqi_color] = level.aqi_color
+      end
       DDataHourlyYyyy.create(hash)
     end
     return status = 1
