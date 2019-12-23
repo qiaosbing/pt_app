1c18148ee2f7a1d98d43d73b14c9d035de64c694
qiao_1998 <13014721444@163.com>
2019-12-23 19:29:29 +0800

    fix：cehis

--- app/jobs/http_tool_job.rb
+++ app/jobs/http_tool_job.rb
@@ -3,8 +3,8 @@ class HttpToolJob < ActiveJob::Base
 
   def perform(*args)
     datas = RestClient.get 'http://106.54.8.7:8080/data_hour'
-    req_data = datas.body
+    req_data = JSON.parse(datas.body)
+
     Rails.logger.info "=====#{req_data.inspect}"
   end
-
   end
