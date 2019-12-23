35dc65a561d67fe17f6dfc9fd1de575a2ed64d0b
qiao_1998 <13014721444@163.com>
2019-12-23 18:39:44 +0800

    fix：测试

--- app/jobs/http_tool_job.rb
+++ app/jobs/http_tool_job.rb
@@ -2,7 +2,7 @@ class HttpToolJob < ActiveJob::Base
 
 
   def perform(*args)
-    datas = RestClient.get 'http://127.0.0.1:8080/data_min'
+    datas = RestClient.get 'http://106.54.8.7:8080/data_min'
     req_data = datas.body
     Rails.logger.info "=====#{req_data.inspect}"
   end
