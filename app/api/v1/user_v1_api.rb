5a78461724fa991446e674cc287b940626497aad
qiao_1998 <13014721444@163.com>
2019-12-23 10:45:48 +0800

    fix：测试

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -43,8 +43,8 @@ class UserV1Api < Grape::API
               avg_tvoc: x["avg_tvoc"],tvoc_label:x["tvoc_label"]}
 
       #获取aqi数据
-      aqi = x["aqi"].to_f
-      if aqi.present? && aqi > 0
+      aqi = x["aqi"].to_s
+      if aqi.present? && aqi != nil
         Rails.logger.info "=====#{aqi}"
         level = SAqiLevel.find_by_aqi(aqi)
         hash[:aqi] = aqi
