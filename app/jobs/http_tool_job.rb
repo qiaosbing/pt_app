4c8fdbae6cd8d617d520a57078b76b3f52204183
qiao_1998 <13014721444@163.com>
2019-12-23 14:58:10 +0800

    fix：测试

--- app/jobs/http_tool_job.rb
+++ app/jobs/http_tool_job.rb
@@ -2,10 +2,9 @@ class HttpToolJob < ActiveJob::Base
 
 
   def perform(*args)
-    datas = RestClient.post 'http://106.54.8.7:8080/data_hour'
+    datas = RestClient.get 'http://106.54.8.7:8080/data_hour'
     req_data = datas.body
     Rails.logger.info "=====#{req_data.inspect}"
-
   end
 
   end
