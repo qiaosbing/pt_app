07a7391643964d03e3742f9487b981367d9ed1fb
qiao_1998 <13014721444@163.com>
2019-12-23 10:41:00 +0800

    fix：测试

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -44,7 +44,7 @@ class UserV1Api < Grape::API
 
       #获取aqi数据
       aqi = x["aqi"]
-      if x["aqi"] != nil && !x["aqi"].present?
+      if aqi.present?
         Rails.logger.info "=====#{aqi}"
         level = SAqiLevel.find_by_aqi(aqi)
         hash[:aqi] = aqi
