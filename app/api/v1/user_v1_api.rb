4175df44fa27f2105a970c0f344f2d49e30119fe
qiao_1998 <13014721444@163.com>
2019-12-23 10:05:51 +0800

    fix：测试

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -20,7 +20,6 @@ class UserV1Api < Grape::API
     user_name = params[:params]
     return {data_err: "数据为空！"} if user_name.blank? #数据为空则返回[]
     d_station = DStation.where(:station_type => "15") #获取站点信息
-    Rails.logger.info "========#{d}"
     @data_arr = []
     user_name.each do |x|
       hour = DDataHourlyYyyy.new
