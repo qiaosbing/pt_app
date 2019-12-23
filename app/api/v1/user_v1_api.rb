0ffad0d42f73e2db2f21889da3ce8749aef5df43
qiao_1998 <13014721444@163.com>
2019-12-23 10:10:14 +0800

    fix：测试

--- app/api/v1/user_v1_api.rb
+++ app/api/v1/user_v1_api.rb
@@ -22,50 +22,43 @@ class UserV1Api < Grape::API
     d_station = DStation.where(:station_type => "15") #获取站点信息
     @data_arr = []
     user_name.each do |x|
-      hour = DDataHourlyYyyy.new
       station_id = x["station_id"]
       station = d_station.select { |x| x.dz_station_id == station_id }.first
-      Rails.logger.info "========#{station.inspect}"
-      #station = DStation.find_by(:dz_station_id => x["station_id"])
       next if !station.present?
-      hour.station_id = station["id"].to_s
-      hour.station_name = station["station_name"]
-      hour.data_time = x["data_time"]
-      hour.save
-      # hash = {}
-      # next if !station.present?
-      # hash[:station_id] = station.id
-      # hash[:station_name] = station.station_name
-      # hash[:avg_so2] = x["so2"]
-      # hash[:so2_label] = x["so2_label"]
-      # hash[:iaqi_so2] = x["iaqi_so2"]
-      # hash[:avg_no2] = x["no2"]
-      # hash[:no2_label] = x["no2_label"]
-      # hash[:iaqi_no2] = x["iaqi_no2"]
-      # hash[:avg_co] = x["co"]
-      # hash[:co_label] = x["co_label"]
-      # hash[:iaqi_co] = x["iaqi_co"]
-      # hash[:avg_o3] = x["o3"]
-      # hash[:o3_label] = x["o3_label"]
-      # hash[:iaqi_o3] = x["iaqi_o3"]
-      # hash[:avg_pm10] = x["pm10"]
-      # hash[:pm10_label] = x["pm10_label"]
-      # hash[:iaqi_ma24_pm10] = x["iaqi_pm10"]
-      # hash[:avg_pm25] = x["pm25"]
-      # hash[:pm25_label] = x["pm25_label"]
-      # hash[:iaqi_ma24_pm25] = x["iaqi_pm25"]
-      # hash[:temperature] = x["temperature"]
-      # hash[:temperature_label] = x["temperature_label"]
-      # hash[:humidity] = x["humidity"]
-      # hash[:humidity_label] = x["humidity_label"]
-      # hash[:windspeed] = x["windspeed"]
-      # hash[:windspeed_label] = x["windspeed_label"]
-      # hash[:winddirection] = x["winddirection"]
-      # hash[:winddirection_label] = x["winddirection_label"]
-      # hash[:avg_tvoc] = x["avg_tvoc"]
-      # hash[:tvoc_label] = x["tvoc_label"]
-      # hash[:aqi] = x["aqi"]
-      # Rails.logger.info "=====#{hash.inspect}"
+      hash = {}
+      hash[:station_id] = station.id.to_s
+      hash[:station_name] = station.station_name
+      hash[:data_time] = x["data_time"]
+      hash[:avg_so2] = x["so2"]
+      hash[:so2_label] = x["so2_label"]
+      hash[:iaqi_so2] = x["iaqi_so2"]
+      hash[:avg_no2] = x["no2"]
+      hash[:no2_label] = x["no2_label"]
+      hash[:iaqi_no2] = x["iaqi_no2"]
+      hash[:avg_co] = x["co"]
+      hash[:co_label] = x["co_label"]
+      hash[:iaqi_co] = x["iaqi_co"]
+      hash[:avg_o3] = x["o3"]
+      hash[:o3_label] = x["o3_label"]
+      hash[:iaqi_o3] = x["iaqi_o3"]
+      hash[:avg_pm10] = x["pm10"]
+      hash[:pm10_label] = x["pm10_label"]
+      hash[:iaqi_ma24_pm10] = x["iaqi_pm10"]
+      hash[:avg_pm25] = x["pm25"]
+      hash[:pm25_label] = x["pm25_label"]
+      hash[:iaqi_ma24_pm25] = x["iaqi_pm25"]
+      hash[:temperature] = x["temperature"]
+      hash[:temperature_label] = x["temperature_label"]
+      hash[:humidity] = x["humidity"]
+      hash[:humidity_label] = x["humidity_label"]
+      hash[:windspeed] = x["windspeed"]
+      hash[:windspeed_label] = x["windspeed_label"]
+      hash[:winddirection] = x["winddirection"]
+      hash[:winddirection_label] = x["winddirection_label"]
+      hash[:avg_tvoc] = x["avg_tvoc"]
+      hash[:tvoc_label] = x["tvoc_label"]
+      hash[:aqi] = x["aqi"]
+      DDataHourlyYyyy.create(hash)
     end
     return status = 1
   end
